a2215086c3f4651106c83ef54789451250acb1a2
yohansamitha <65862807+yohansamitha@users.noreply.github.com>
2020-09-17 01:50:36 +0530

    V.0.1.1(Finish Main 3 UI)

--- src/controller/ManageSchedulesFormController.java
+++ src/controller/ManageSchedulesFormController.java
@@ -1,4 +1,15 @@
 package controller;
 
+import com.jfoenix.controls.JFXDatePicker;
+import com.jfoenix.controls.JFXTimePicker;
+import javafx.event.ActionEvent;
+
 public class ManageSchedulesFormController {
+    public JFXDatePicker cblSelectDate;
+    public JFXTimePicker cblSelectDepTime;
+
+    public void btnTesting(ActionEvent actionEvent) {
+        System.out.println(cblSelectDate.getValue());
+        System.out.println(cblSelectDepTime.getValue());
+    }
 }
